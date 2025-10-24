e4b604e8391e4a0501f29ef2af36364092a7ac3f
Christian <kalonjic34@gmail.com>
2025-10-24 12:22:10 +0200

    Rename Items to Item and add __str__ method

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
-class Items(models.Model):
+class Item(models.Model):
+    
+    def __str__(self):
+        return self.item_name
+    
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField()
     item_price = models.IntegerField()
